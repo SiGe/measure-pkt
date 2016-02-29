def90ffb340be6f0fdcd5235f9687d7b79aeba16
Omid <a@omid.io>
2016-02-29 15:01:53 -0800

    uh ... forgot to actually commit the modules

---
+++ src/module.h
@@ -0,0 +1,40 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Omid Alipourfard <g@omid.io>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _MODULE_H_
+#define _MODULE_H_
+
+#include "net.h"
+
+typedef struct Module* ModulePtr;
+typedef void (*ModuleFunc)(ModulePtr, PortPtr, \
+        struct rte_mbuf ** __restrict__, uint32_t);
+
+struct Module {
+    const char name[MAX_MODULE_NAME];
+
+    ModuleFunc execute;
+};
+
+#endif // _MODULE_H_
